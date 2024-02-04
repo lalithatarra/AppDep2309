e876783ed700036aa3101dfebb70dddddad8ce67
lalitha <lalithatarra147@gmail.com>
2024-02-04 17:57:51 +0530

    initial commit

---
+++ client/src/components/EditProfile.js
@@ -0,0 +1,112 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom';
+import TopNavigation from './TopNavigation';
+import { useSelector } from 'react-redux';
+
+function EditProfile() {
+let firstNameInputRef=useRef();
+let lastNameInputRef=useRef();
+let ageInputRef=useRef();
+let emailInputRef=useRef();
+let passwordInputRef=useRef();
+let profilePicInputRef=useRef();
+let[profilePicPath,setProfilePicPath]=useState("./images/download.png");
+
+let storeObj=useSelector((store)=>{
+    return store;
+});
+
+useEffect(()=>{
+   firstNameInputRef.current.value=storeObj.loginDetails.firstName;
+   lastNameInputRef.current.value=storeObj.loginDetails.lastName;
+   ageInputRef.current.value=storeObj.loginDetails.age;
+   emailInputRef.current.value=storeObj.loginDetails.email;
+   setProfilePicPath(`http://localhost:4567/${storeObj.loginDetails.profilePic}`)
+},[]);
+
+
+let sendUpdateDataToServerThruFD=async()=>{
+
+    let dataToSend=new FormData();
+    dataToSend.append("firstName",firstNameInputRef.current.value);
+    dataToSend.append("lastName",lastNameInputRef.current.value);
+    dataToSend.append("age",ageInputRef.current.value);
+    dataToSend.append("email",emailInputRef.current.value);
+    dataToSend.append("password",passwordInputRef.current.value);
+
+    for(let i=0;i<profilePicInputRef.current.files.length;i++){
+
+        dataToSend.append("profilePic",profilePicInputRef.current.files[i]);
+    };
+    
+
+    let reqOptions={
+        method:"PUT",
+        body:dataToSend,
+    };
+
+    let JSONData=await fetch("http://localhost:4567/updateProfile",reqOptions);
+
+    let JSOData=await JSONData.json();
+
+    if(JSOData.status=="success"){
+        alert(JSOData.msg);
+    }else{
+        alert(JSOData.msg);
+    }
+    console.log(JSOData);
+};
+
+
+
+  return (
+    <div className='App'>
+        <TopNavigation></TopNavigation>
+        <form>
+            <h2 >Edit Profile</h2>
+            <div>
+                <label className='profilePic'>profile Pic</label>
+                <input ref={profilePicInputRef} type='file' onChange={(eventObj)=>{
+                    let selectedImagePath=URL.createObjectURL(eventObj.target.files[0]);
+                   setProfilePicPath(selectedImagePath);
+                 }}></input>
+            </div>
+            <div>
+                <img className='profilePic' src={profilePicPath}></img>
+            </div>
+            <div>
+                <label>First Name</label>
+                <input ref={firstNameInputRef} placeholder='enter your first name'></input>
+            </div>
+            <div>
+                <label>Last Name</label>
+                <input ref={lastNameInputRef} placeholder='enter your last name'></input>
+            </div>
+            <div>
+                <label>Age</label>
+                <input ref={ageInputRef} placeholder='enter your age'></input>
+            </div>
+            <div>
+                <label>Email</label>
+                <input ref={emailInputRef} readOnly placeholder='enter your email'></input>
+            </div>
+            <div>
+                <label>Password</label>
+                <input ref={passwordInputRef} placeholder='enter your password'></input>
+            </div>
+            
+            <div>
+                
+                <button type='button' onClick={()=>{
+                   sendUpdateDataToServerThruFD();
+                }}>Update Profile</button>
+            </div>
+        </form>
+       
+    </div>
+    
+
+  )
+}
+
+export default EditProfile;
---
+++ client/src/components/SignUp.js
@@ -0,0 +1,152 @@
+import React, { useRef, useState } from 'react'
+import { Link } from 'react-router-dom';
+
+function SignUp() {
+let firstNameInputRef=useRef();
+let lastNameInputRef=useRef();
+let ageInputRef=useRef();
+let emailInputRef=useRef();
+let passwordInputRef=useRef();
+let profilePicInputRef=useRef();
+let[profilePicPath,setProfilePicPath]=useState("./images/download.png");
+
+let sendSignupDataToServerThruJSON=async()=>{
+
+    let dataToSend={
+        firstName:firstNameInputRef.current.value,
+        lastName:lastNameInputRef.current.value,
+        age:ageInputRef.current.value,
+        email:emailInputRef.current.value,
+        password:passwordInputRef.current.value,
+        profilePic:profilePicInputRef.current.value,
+
+    };
+    let dataToSendInJSON=JSON.stringify(dataToSend);
+     
+     let myHeader=new Headers();
+     myHeader.append("content-type","application/json");
+
+    let reqOptions={
+        method:"POST",
+        body:dataToSendInJSON,
+        headers:myHeader,
+    };
+
+   let JSONData=await fetch("http://localhost:4567/signup",reqOptions);
+   
+   let JSOData=await JSONData.json();
+   console.log(JSOData);
+};
+
+
+let sendSignupDataToServerThruURLE=async()=>{
+
+    let dataToSend=new URLSearchParams();
+    dataToSend.append("firstName",firstNameInputRef.current.value);
+    dataToSend.append("lastName",lastNameInputRef.current.value);
+    dataToSend.append("age",ageInputRef.current.value);
+    dataToSend.append("email",emailInputRef.current.value);
+    dataToSend.append("password",passwordInputRef.current.value);
+    dataToSend.append("profilePic",profilePicInputRef.current.value);
+
+   let myHeader=new Headers();
+   myHeader.append("content-type","application/x-www-form-urlencoded");
+    let reqOptions={
+        method:"POST",
+        body:dataToSend,
+        headers:myHeader,
+    };
+
+    let JSONData=await fetch("http://localhost:4567/signup",reqOptions);
+
+    let JSOData=await JSONData.json();
+    console.log(JSOData);
+};
+
+
+let sendSignupDataToServerThruFD=async()=>{
+
+    let dataToSend=new FormData();
+    dataToSend.append("firstName",firstNameInputRef.current.value);
+    dataToSend.append("lastName",lastNameInputRef.current.value);
+    dataToSend.append("age",ageInputRef.current.value);
+    dataToSend.append("email",emailInputRef.current.value);
+    dataToSend.append("password",passwordInputRef.current.value);
+
+    for(let i=0;i<profilePicInputRef.current.files.length;i++){
+
+        dataToSend.append("profilePic",profilePicInputRef.current.files[i]);
+    };
+    
+
+    let reqOptions={
+        method:"POST",
+        body:dataToSend,
+    };
+
+    let JSONData=await fetch("http://localhost:4567/signup",reqOptions);
+
+    let JSOData=await JSONData.json();
+
+    if(JSOData.status=="success"){
+        alert(JSOData.msg);
+    }else{
+        alert(JSOData.msg);
+    }
+    console.log(JSOData);
+};
+
+
+
+  return (
+    <div className='App'>
+        <form>
+            <h2 >Sign Up</h2>
+            <div>
+                <label className='profilePic'>profile Pic</label>
+                <input ref={profilePicInputRef} type='file' onChange={(eventObj)=>{
+                    let selectedImagePath=URL.createObjectURL(eventObj.target.files[0]);
+                   setProfilePicPath(selectedImagePath);
+                 }}></input>
+            </div>
+            <div>
+                <img className='profilePic' src={profilePicPath}></img>
+            </div>
+            <div>
+                <label>First Name</label>
+                <input ref={firstNameInputRef} placeholder='enter your first name'></input>
+            </div>
+            <div>
+                <label>Last Name</label>
+                <input ref={lastNameInputRef} placeholder='enter your last name'></input>
+            </div>
+            <div>
+                <label>Age</label>
+                <input ref={ageInputRef} placeholder='enter your age'></input>
+            </div>
+            <div>
+                <label>Email</label>
+                <input ref={emailInputRef} placeholder='enter your email'></input>
+            </div>
+            <div>
+                <label>Password</label>
+                <input ref={passwordInputRef} placeholder='enter your password'></input>
+            </div>
+            
+            <div>
+                
+                <button type='button' onClick={()=>{
+                    sendSignupDataToServerThruFD();
+                }}>Sign Up(Form Data)</button>
+            </div>
+        </form>
+        <div>
+       <Link to="/">Login</Link>
+       </div>
+    </div>
+    
+
+  )
+}
+
+export default SignUp
